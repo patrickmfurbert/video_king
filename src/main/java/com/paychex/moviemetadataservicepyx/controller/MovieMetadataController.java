3907e355ff2aabe7a3b8c7c7ab67d2ddd8d1cdb4
Patrick Furbert <patrickmfurbert@gmail.com>
2022-01-31 22:59:36 -0500

    post mapping

--- src/main/java/com/paychex/moviemetadataservicepyx/controller/MovieMetadataController.java
+++ src/main/java/com/paychex/moviemetadataservicepyx/controller/MovieMetadataController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,6 +52,9 @@ public class MovieMetadataController {
 
     // Post Mapping
     @PostMapping(value = "/movies")
-    
+    public List<MovieMetadata> addMovie(@RequestBody MovieMetadata movie){
+        List<MovieMetadata> movieMetadata = movieMetadataService.create(movie);
+        return movieMetadata;
+    }
     
 }
